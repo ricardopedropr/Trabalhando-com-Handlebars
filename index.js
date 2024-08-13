541a65ad3108108797dbd535cdab561f661a9acc
ricardopedropr <ricardopedro.pr@gmail.com>
2024-08-12 23:11:24 -0300

    index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.set('view engine', 'handlebars');
 //router.get('/about', function(req,res){
 //    res.sendFile(path.join(__dirname+'/about.html'));
 //})
-app.use('/', routesAdmin);
+app.use('/', routesAdmin)
 //    app.use('/about', router);
 app.listen(process.env.port||3000);
 console.log("Servidor rodando")
